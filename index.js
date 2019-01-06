fdef8f0674922819d22c95c37bccb3ba5d44f6fd
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-01-06 19:46:17 +0000

    handle file passed as parameter considering the current directory

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const File = require('./lib/file');
 
-const params = require('./lib/params')(process.argv);
+const params = require('./lib/params')(process.argv, process.cwd());
 const file = new File(params);
 const data = file.read();
 file.parseAndUpdate(data);
