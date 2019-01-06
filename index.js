371068fbba514f30869ee687bdfee507ae50ce8c
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-01-06 20:49:53 +0000

    fix problems to create a new file

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ const File = require('./lib/file');
 
 const params = require('./lib/params')(process.argv, process.cwd());
 const file = new File(params);
-const data = file.read();
-file.parseAndUpdate(data);
+file.read(file.parseAndUpdate.bind(file));
