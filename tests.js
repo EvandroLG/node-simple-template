1d90a7ac341938b0a3e71c215841357a4e561712
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-01-19 14:30:37 +0000

    add tests to validation component

--- tests.js
+++ tests.js
@@ -1,24 +1,58 @@
 const path = require('path');
 const assert = require('assert');
 const params = require('./lib/params');
+const validation = require('./lib/validation');
 
 const initialFixture = ['/usr/local/bin/node', __dirname];
 
-describe('params', () => {
-    function verify(args) {
-        const dir = 'src';
-        const actual = params([...initialFixture, ...args], dir);
-        const expected = {
-            '-f': path.join(dir, 'index.html'),
-            '-c': path.join(dir, 'config.json'),
-            '-d': path.join(dir, 'dist')
-        };
+describe('simple-template', () => {
+    describe('params', () => {
+        it('should return an object with correct key-value structure', () => {
+            const dir = 'src';
+            const args = ['-f', 'index.html', '-c', 'config.json', '-d', 'dist'];
+            const actual = params([...initialFixture, ...args], dir);
+            const expected = {
+                '-f': path.join(dir, 'index.html'),
+                '-c': path.join(dir, 'config.json'),
+                '-d': path.join(dir, 'dist')
+            };
 
-        assert.deepEqual(actual, expected);
-    }
+            assert.deepEqual(actual, expected);
+        });
+    });
 
-    it('should return an object with correct key-value structure', () => {
-        const args = ['-f', 'index.html', '-c', 'config.json', '-d', 'dist'];
-        verify(args);
+    describe('validation', () => {
+        function verify(args, isValid) {
+            const cachedExit = process.exit;
+            let wasExitCalled = false;
+            process.exit = () => {
+                wasExitCalled = true;
+            };
+
+            const cachedConsole = console.log;
+            let wasConsoleCalled = false;
+            console.log = () => {
+                wasConsoleCalled = true;
+            };
+
+            validation(args);
+
+            console.log = cachedConsole;
+            process.exit = cachedExit;
+
+
+            console.log(cachedConsole);
+
+            assert.equal(wasExitCalled, !isValid);
+            assert.equal(wasConsoleCalled, !isValid);
+        }
+
+        it('should not call any function when validation does not have any problem', () => {
+            verify(['-f', 'index.html', '-c', 'config.json', '-d', 'dist'], true);
+        });
+
+        it('should print an message error and finish the process when validation fails', () => {
+            verify(['-f', 'index.html', '-c', 'config.json'], false);
+        });
     });
 });
