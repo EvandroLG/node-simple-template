255ecb0b8e827e1bbc302767e36ea1e53d7aeb6d
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-01-20 21:07:29 +0000

    add copyright in index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+#!/usr/bin/env node
+
+/*
+  * simple-template
+  * http://github.com/EvandroLG/node-compressor
+  * author: Evandro Leopoldino Goncalves <evandrolgoncalves@gmail.com>
+  * http://github.com/evandrolg
+  * License: MIT
+*/
+
 const File = require('./lib/file');
 
 const params = require('./lib/params')(process.argv, process.cwd());
